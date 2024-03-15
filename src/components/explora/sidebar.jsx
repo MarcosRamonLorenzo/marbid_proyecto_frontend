62c8132ffb0f874a8276c9f5f084e0fe84c40584
Marcos <marcosramon.alu@iespacomolla.es>
2024-03-15 13:25:02 +0100

    Add lucide-react library and update sidebar component

--- src/components/explora/sidebar.jsx
+++ src/components/explora/sidebar.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { MoreVertical, ChevronLast, ChevronFirst } from "lucide-react";
+import { useContext, createContext, useState } from "react";
+import { UserButton, useUser } from "@clerk/clerk-react";
+const SidebarContext = createContext();
 
-const sidebar = () => {
-  return <div>sidebar</div>;
+const Sidebar = ({ children }) => {
+  const [expanded, setExpanded] = useState(true);
+  const { user } = useUser();
+
+  return (
+    <aside className="h-screen">
+      <nav className="h-full  flex flex-col bg-white border-r shadow-sm">
+        <div className="p-4 pb-2 flex  items-center">
+          <img
+            src="/src/assets/marbid.svg "
+            className={`overflow-hidden transition-all ${
+              expanded ? "w-10 mr-5" : "w-0"
+            }`}
+            alt=""
+          />
+          <button
+            onClick={() => setExpanded((curr) => !curr)}
+            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+          >
+            {expanded ? <ChevronFirst /> : <ChevronLast />}
+          </button>
+        </div>
+
+        <SidebarContext.Provider value={{ expanded }}>
+          <ul className="flex-1 px-3">{children}</ul>
+        </SidebarContext.Provider>
+
+        <div className="border-t flex p-3">
+          <UserButton afterSignOutUrl="/" />
+          <div
+            className={`
+          flex justify-between items-center
+          overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}
+      `}
+          >
+            <div className="leading-4">
+              <h4 className="font-semibold">
+                {/*  {user.externalAccounts[0].firstName} */}
+              </h4>
+              {console.log(user)}
+              <span className="text-xs text-gray-600">
+                {user.primaryEmailAddress.emailAddress}
+              </span>
+            </div>
+            <MoreVertical size={20} />
+          </div>
+        </div>
+      </nav>
+    </aside>
+  );
 };
 
-export default sidebar;
+export default Sidebar;
+export { SidebarContext };
--- src/pages/Explora.jsx
+++ src/pages/Explora.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Sidebar from "../components/explora/Sidebar.jsx";
+import SidebarItem from "../components/explora/SidebarItem.jsx";
+import { LifeBuoy, Receipt, Boxes, Package } from "lucide-react";
+import { Outlet } from "react-router-dom";
 
 const Explora = () => {
-  return <div>Explora</div>;
+  return (
+    <div className="flex">
+      <div className="">
+        <Sidebar>
+          <SidebarItem icon={<LifeBuoy />} text="Soporte" />
+          <SidebarItem icon={<Receipt />} text="Facturas" />
+          <SidebarItem icon={<Boxes />} text="Productos" />
+          <SidebarItem icon={<Package />} text="Envíos" />
+        </Sidebar>
+      </div>
+      <div className="">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Explora;
