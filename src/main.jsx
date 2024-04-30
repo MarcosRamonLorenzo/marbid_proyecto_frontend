b46b2307cee31a25ffc73f0b3443b6f8dfd3fb18
xXxXxDavidxXxXx <davidcasanyez.alu@iespacomolla.es>
2024-04-30 20:03:22 +0200

    Cambio index.css a index.scss e instalo Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/AuthProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
