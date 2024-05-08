3a3754acee77ab88be055ef364b7d545a73c444e
MarcosRamonLorenzo$ git config --get-all user.name <marcosramon.alu@iespacomolla.es>
2024-05-09 00:06:54 +0200

    link para inicio y logo

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
 import Header from "../components/shared-componentes/Header.jsx";
 import "./scss/NotFound.scss";
+import { Button,Image } from "@nextui-org/react";
 
 const NotFound = () => {
   return (
     <>
       <Header />
       <div className="container-not-found">
-        <div className="text-center">
+        <div className="flex flex-col items-center justify-center">
+          <Image src="\src\assets\marbid.svg" alt="404" width={80} /> 
           <h1>Página no encontrada</h1>
           <p>Lo sentimos, la página que estás buscando no existe.</p>
+          <Link to="/"><Button className="third-color-class text-white mt-4">Volver al inicio</Button></Link>
         </div>
       </div>
     </>
