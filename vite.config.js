f0045b31daa728a5cc30c895c9a678c84ccc6ae9
MarcosRamonLorenzo$ git config --get-all user.name <marcosramon.alu@iespacomolla.es>
2024-05-28 23:22:44 +0200

    chore: Update vite.config.js for build configuration
    This commit updates the vite.config.js file to include build configuration options. The `outDir` is set to 'dist' and the `assetsDir` is set to 'assets'. This change allows for a more organized output structure for the built assets.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Esto es opcional si quieres cambiar la carpeta de salida de assets
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
