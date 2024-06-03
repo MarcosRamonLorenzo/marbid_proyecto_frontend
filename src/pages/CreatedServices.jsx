3c0c3a2dffbdf2a284924392e1a51a3680344b85
MarcosRamonLorenzo$ git config --get-all user.name <marcosramon.alu@iespacomolla.es>
2024-06-03 15:20:08 +0200

    support chat and change add service button position

--- src/pages/CreatedServices.jsx
+++ src/pages/CreatedServices.jsx
@@ -38,16 +38,18 @@ const CreatedServices = () => {
 
   return (
     <div className=" created-services mx-4 my-10 md:mx-24 md:my-20">
-      <Button
-        className="flex items-center bg-blue-400 text-white gap-1 px-4 py-2 cursor-pointer  font-semibold tracking-widest rounded-md hover:bg-blue-400 duration-300 hover:gap-2 hover:translate-x-3 fixed right-5 bottom-5 z-30"
-        endContent={<CirclePlus />}
-        onClick={() => {
-          navigate("/panel-control/creacion-servicio");
-        }}
-      >
-        Crear Anuncio
-      </Button>
-      <h2 className="text-3xl font-medium">Servicios Creados</h2>
+      <div className="flex items-center jusatify-center gap-5">
+        <h2 className="text-3xl font-medium">Servicios Creados</h2>
+        <Button
+          className="flex items-center bg-blue-400 text-white gap-1 px-4 py-2 cursor-pointer  font-semibold tracking-widest rounded-md hover:bg-blue-400 duration-300 hover:gap-2 hover:translate-x-3 "
+          endContent={<CirclePlus />}
+          onClick={() => {
+            navigate("/panel-control/creacion-servicio");
+          }}
+        >
+          Crear Anuncio
+        </Button>
+      </div>
       <Divider className="my-4" />
       <h3 className=" text-xl ml-3">Visualización</h3>
       <Tabs aria-label="Options" variant="underlined">
