c5920bd9af643ea3b55ec7d6eaf762e8cd6a87b1
MarcosRamonLorenzo$ git config --get-all user.name <marcosramon.alu@iespacomolla.es>
2024-06-04 20:37:00 +0200

    navigate profile services and remove button

--- src/components/services/CreatedServicesList.jsx
+++ src/components/services/CreatedServicesList.jsx
@@ -1,18 +1,22 @@
 import ServiceCard from "@/components/services/ServiceCard";
 import LoadingCards from "@/components/shared-componentes/Loadings/LoadingCards";
+import { useNavigate } from "react-router-dom";
 
 const CreatedServicesList = ({
   createdServices = [],
   isLoading,
   className = "",
 }) => {
+
+  const navigate = useNavigate();
+
   return (
     <>
       {isLoading && <LoadingCards className={className} />}
       {createdServices.length ? (
         <div className={className}>
           {createdServices.map((item, index) => (
-            <ServiceCard item={item} key={index} />
+            <ServiceCard item={item} key={index} onClick={()=>{navigate(`/servicio/${item?.id}`)}} />
           ))}
         </div>
       ) : (
